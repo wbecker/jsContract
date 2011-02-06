15b0c14b01bb43f6f535f5c39511a73a30832539
William Becker <wbekcer@gmail.com>
2011-02-06 14:15:09 +0000

    Use jsUnit for testing

--- test/Person.js
+++ test/Person.js
@@ -1,100 +1,109 @@
-Person = 
-  new jsContract({
-      constructor: true,
-      invariant: [
-        "name !== null",
-        "name !== undefined",
-        "name.length > 0"
-      ]
-    },
-    function (name) {
-      this.name = name; 
-    }
-  );
-
-Person.prototype.getName = 
-  new jsContract({
-      post: [
-        "result == this.name"
+var personTest = function () {
+  Person = 
+    new jsContract({
+        constructor: true,
+        invariant: [
+          "name !== null",
+          "name !== undefined",
+          "name.length > 0"
+        ]
+      },
+      function (name) {
+        this.name = name; 
+      }
+    );
+  
+  Person.prototype.getName = 
+    new jsContract({
+        post: [
+          "result == this.name"
+        ]
+      },
+      function () {
+        return this.name;
+      }
+    );
+  Person.prototype.setName = 
+    new jsContract({
+        pre: [
+          "name !== null",
+          "name !== undefined",
+          "name.length > 0"
+        ],
+        post: [
+          "this.getName() == name"
+        ]
+      },
+      function (name) {
+        this.name = name;
+      }
+    );
+  
+  Person.prototype.testExceptionOk = 
+    new jsContract ({
+      throwEnsures: [
+        ["SyntaxError", "true"]
       ]
     },
     function () {
-      return this.name;
-    }
-  );
-Person.prototype.setName = 
-  new jsContract({
-      pre: [
-        "name !== null",
-        "name !== undefined",
-        "name.length > 0"
-      ],
-      post: [
-        "this.getName() == name"
+      throw new SyntaxError("bad syntax");
+    });
+  Person.prototype.testExceptionBad = 
+    new jsContract ({
+      throwEnsures: [
+        ["SyntaxError", "true"]
       ]
     },
-    function (name) {
-      this.name = name;
+    function () {
+      throw new Error("bad syntax");
+    });
+  
+  var test = function (f) {
+    try {
+      f();
     }
-  );
-
-Person.prototype.testExceptionOk = 
-  new jsContract ({
-    throwEnsures: [
-      ["SyntaxError", "true"]
-    ]
-  },
-  function () {
-    throw new SyntaxError("bad syntax");
-  });
-Person.prototype.testExceptionBad = 
-  new jsContract ({
-    throwEnsures: [
-      ["SyntaxError", "true"]
-    ]
-  },
-  function () {
-    throw new Error("bad syntax");
-  });
-
-var test = function (f) {
-  try {
-    f();
+    catch (e) {
+      console.debug(e);
+    }
   }
-  catch (e) {
-    console.debug(e);
-  }
+  test(function () {
+    console.debug("Create person with no argument, expect rule to fail");
+    new Person();
+  });
+  var p = new Person("Bob");
+  test(function () {
+    console.debug("getName on a properly instantiated Person should pass");
+    p.getName();
+  });
+  test(function () {
+    console.debug("setName with a null argument will fail");
+    p.setName(null);
+  });
+  test(function () {
+    console.debug("setName with no argument will fail");
+    p.setName();
+  });
+  test(function () {
+    console.debug("setName with an empty argument should fail");
+    p.setName("");
+  });
+  test(function () {
+    console.debug("setName with a non-empty string should pass");
+    p.setName("Jim");
+  });
+  test(function () {
+    console.debug("Calling textExceptionOk should pass");
+    p.testExceptionOk();
+  });
+  test(function () {
+    console.debug("Calling textExceptionBad should fail");
+    p.testExceptionBad();
+  });
 }
-test(function () {
-  console.debug("Create person with no argument, expect rule to fail");
-  new Person();
-});
-var p = new Person("Bob");
-test(function () {
-  console.debug("getName on a properly instantiated Person should pass");
-  p.getName();
-});
-test(function () {
-  console.debug("setName with a null argument will fail");
-  p.setName(null);
-});
-test(function () {
-  console.debug("setName with no argument will fail");
-  p.setName();
-});
-test(function () {
-  console.debug("setName with an empty argument should fail");
-  p.setName("");
-});
-test(function () {
-  console.debug("setName with a non-empty string should pass");
-  p.setName("Jim");
-});
-test(function () {
-  console.debug("Calling textExceptionOk should pass");
-  p.testExceptionOk();
-});
-test(function () {
-  console.debug("Calling textExceptionBad should fail");
-  p.testExceptionBad();
-});
+
+try {
+  personTest();
+}
+catch (e) {
+  console.debug(e);
+}
--- test/time.js
+++ test/time.js
@@ -1,72 +1,74 @@
-var time = function () {
-};
-
-time.prototype.setHour = 
-  new jsContract({
-    pre:[
-      "hour >= 0", 
-      "hour <= 23"
-    ]}, 
-    function (hour) {
-      this.hour = hour;
-    }
-  );
-
-time.prototype.getHour = 
-  new jsContract({
-    post:[
-      "result >= 0", 
-      "result <= 23"
-    ]}, 
+var timeTest = (
+  function () {
+  var time = function () {
+  };
+  
+  time.prototype.setHour = 
+    new jsContract({
+      pre:[
+        "hour >= 0", 
+        "hour <= 23"
+      ]}, 
+      function (hour) {
+        this.hour = hour;
+      }
+    );
+  
+  time.prototype.getHour = 
+    new jsContract({
+      post:[
+        "result >= 0", 
+        "result <= 23"
+      ]}, 
+      function () {
+        return this.hour;
+      }
+    );
+  
+  var t = new time();
+  assertThrowsException("set hour to -1, expect rule to fail", 
     function () {
-      return this.hour;
-    }
-  );
+      t.setHour(-1);
+    });
+  var test = function () {};
+  assertDoesntThrowException("set hour to 0, expect rule to pass", 
+    function () {
+      t.setHour(0);
+    });
+  assertDoesntThrowException("set hour to 23, expect rule to pass", 
+    function () {
+      t.setHour(23);
+    });
+  assertThrowsException("set hour to 24, expect rule to fail", 
+    function () {
+      t.setHour(24);
+    });
 
-var t = new time();
-var test = function (f) {
-  try {
-    f();
-  }
-  catch (e) {
-    console.debug(e);
-  }
+  assertThrowsException("get hour when hour is -1, expect rule to fail", 
+    function () {
+      t.hour=-1;
+      t.getHour();
+    });
+  assertDoesntThrowException("get hour when hour is 0, expect rule to pass", 
+    function () {
+      t.hour=0;
+      t.getHour();
+    });
+  assertDoesntThrowException("get hour when hour is 23, expect rule to pass", 
+    function () {
+      t.hour=23;
+      t.getHour();
+    });
+  assertThrowsException("get hour when hour is 24, expect rule to fail", 
+    function () {
+      t.hour=24;
+      t.getHour();
+    });
+});
+
+try {
+  timeTest();
 }
-
-test(function () {
-  console.debug("set hour to -1, expect rule to fail");
-  t.setHour(-1);
-});
-test(function () {
-  console.debug("set hour to 0, expect rule to pass");
-  t.setHour(0);
-});
-test(function () {
-  console.debug("set hour to 23, expect rule to pass");
-  t.setHour(23);
-});
-test(function () {
-  console.debug("set hour to 24, expect rule to fail");
-  t.setHour(24);
-});
-
-test(function () {
-  t.hour=-1;
-  console.debug("get hour when hour is -1, expect rule to fail");
-  t.getHour();
-});
-test(function () {
-  t.hour=0;
-  console.debug("get hour when hour is 0, expect rule to pass");
-  t.getHour();
-});
-test(function () {
-  t.hour=23;
-  console.debug("get hour when hour is 23, expect rule to pass");
-  t.getHour();
-});
-test(function () {
-  t.hour=24;
-  console.debug("get hour when hour is 24, expect rule to fail");
-  t.getHour();
-});
+catch(e) {
+  console.debug(e);
+}
