09c25913b19fd8f4eed441bbbfc7f48a689b3573
William Becker <wbekcer@gmail.com>
2011-02-06 17:41:14 +0000

    Tidy up order

--- src/contracts.js
+++ src/contracts.js
@@ -24,10 +24,63 @@ jsContract.prototype.applyContract = function (rules, fn) {
   return this.applyRules(fn, isConstructor, preRules, postRules, invariantRules, 
     throwRules);
 };
+jsContract.prototype.getParamMap = function (fn) {
+  var reg, params, paramNames, paramMap;
+  reg = /\(([\s\S]*?)\)/;
+  params = reg.exec(fn);
+  if (params && (params[1] !== "")) {
+    paramNames = params[1].split(',');
+  }
+  else {
+    paramNames = [];
+  }
+  paramMap = {};
+  paramNames.forEach(function (paramName, index) {
+    paramMap[paramName] = index; 
+  });
+  return paramMap;
+};
 jsContract.prototype.processRuleSet = function (ruleSet, ruleProcessor) {
   var processedRules, ruleProcessorToApply;
   ruleProcessorToApply = ruleProcessor ? ruleProcessor : this.processRule;
   return ruleSet ?  ruleSet.map(ruleProcessorToApply, this) : [];
+};
+jsContract.prototype.processRule = function (rule) {
+  var transformedRule = this.transformRule(rule);
+  return this.applyRule(rule, transformedRule);
+};
+jsContract.prototype.applyRule = function (rule, transformedRule) {
+  return function (args, result) {
+    var ruleResult = eval(transformedRule);
+    if (!ruleResult) {
+      throw "Rule failed: "+rule;
+    }
+  };
+};
+jsContract.prototype.processThrowRule = function (rule) {
+  var transformedRule = this.transformRule(rule[1]);
+  return this.applyExceptionRule(rule[1], rule[0], transformedRule);
+};
+jsContract.prototype.applyExceptionRule = function (
+    rule, exceptionType, transformedRule) {
+  return function (args, ex) {
+    var ruleResult = eval(transformedRule);
+    if (ruleResult) {
+      if (!(eval("ex instanceof "+exceptionType))) {
+        throw "Rule failed: when "+rule+", throw "+exceptionType;
+      }
+    }
+  };
+};
+jsContract.prototype.transformRule = function (rule) {
+  var paramName, regEx, transformedRule;
+  transformedRule = rule;
+  for (paramName in this.paramMap) {
+    regEx = new RegExp(paramName, "g");
+    transformedRule = transformedRule.replace(regEx, 
+      "args["+this.paramMap[paramName]+"]");
+  }
+  return transformedRule;
 };
 jsContract.prototype.applyRules = function (
     fn, isConstructor, preRules, postRules, invariantRules, throwRules) {
@@ -60,57 +113,4 @@ jsContract.applyRuleSet = function (ruleSet, that, args, result) {
   ruleSet.forEach(function (rule) {
     rule.apply(that, [args, result]);
   });
-};
-jsContract.prototype.getParamMap = function (fn) {
-  var reg, params, paramNames, paramMap;
-  reg = /\(([\s\S]*?)\)/;
-  params = reg.exec(fn);
-  if (params && (params[1] !== "")) {
-    paramNames = params[1].split(',');
-  }
-  else {
-    paramNames = [];
-  }
-  paramMap = {};
-  paramNames.forEach(function (paramName, index) {
-    paramMap[paramName] = index; 
-  });
-  return paramMap;
-};
-jsContract.prototype.processRule = function (rule) {
-  var transformedRule = this.transformRule(rule);
-  return this.applyRule(rule, transformedRule);
-};
-jsContract.prototype.processThrowRule = function (rule) {
-  var transformedRule = this.transformRule(rule[1]);
-  return this.applyExceptionRule(rule[1], rule[0], transformedRule);
-};
-jsContract.prototype.transformRule = function (rule) {
-  var paramName, regEx, transformedRule;
-  transformedRule = rule;
-  for (paramName in this.paramMap) {
-    regEx = new RegExp(paramName, "g");
-    transformedRule = transformedRule.replace(regEx, 
-      "args["+this.paramMap[paramName]+"]");
-  }
-  return transformedRule;
-};
-jsContract.prototype.applyRule = function (rule, transformedRule) {
-  return function (args, result) {
-    var ruleResult = eval(transformedRule);
-    if (!ruleResult) {
-      throw "Rule failed: "+rule;
-    }
-  };
-};
-jsContract.prototype.applyExceptionRule = function (
-    rule, exceptionType, transformedRule) {
-  return function (args, ex) {
-    var ruleResult = eval(transformedRule);
-    if (ruleResult) {
-      if (!(eval("ex instanceof "+exceptionType))) {
-        throw "Rule failed: when "+rule+", throw "+exceptionType;
-      }
-    }
-  };
 };
